027cafd129052df51919cd896396689066d36112
Douglas Modolo <douglasmodolo@outlook.com.br>
2026-04-29 21:44:03 -0300

    add jpa annotations and create repository

---
+++ src/main/java/br/com/douglas/repository/ExchangeRepository.java
@@ -0,0 +1,9 @@
+package br.com.douglas.repository;
+
+import br.com.douglas.model.Exchange;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExchangeRepository extends JpaRepository<Exchange, Long> {
+
+    Exchange findByFromAndTo(String from, String to);
+}
