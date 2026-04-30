9ae9d1ab4ab83d9b0b4a705e66d5b2abb2d212e8
Douglas Modolo <douglasmodolo@outlook.com.br>
2026-04-29 21:14:13 -0300

    modify controller to get port

--- src/main/java/br/com/douglas/controller/ExchangeController.java
+++ src/main/java/br/com/douglas/controller/ExchangeController.java
@@ -1,6 +1,8 @@
 package br.com.douglas.controller;
 
+import br.com.douglas.environment.InstanceInformationService;
 import br.com.douglas.model.Exchange;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,10 +15,13 @@ import java.math.BigDecimal;
 @RequestMapping("exchange-service")
 public class ExchangeController {
 
+    @Autowired
+    InstanceInformationService informationService;
+
     // http://localhost:8000/exchange-service/5/USD/BRL
     @GetMapping(value = "/{amount}/{from}/{to}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Exchange getExchange(@PathVariable("amount") BigDecimal amount,@PathVariable("from") String from,@PathVariable("to") String to)
     {
-        return new Exchange(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, "PORT 8000");
+        return new Exchange(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, "PORT " + informationService.retrieveServerPort());
     }
 }
