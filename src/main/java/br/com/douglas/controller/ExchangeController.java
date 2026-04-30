4c93b3889942b4b8c3fc08dd269a3b22693bbd19
Douglas Modolo <douglasmodolo@outlook.com.br>
2026-04-29 21:50:11 -0300

    remove mock from exchange controller

--- src/main/java/br/com/douglas/controller/ExchangeController.java
+++ src/main/java/br/com/douglas/controller/ExchangeController.java
@@ -2,6 +2,7 @@ package br.com.douglas.controller;
 
 import br.com.douglas.environment.InstanceInformationService;
 import br.com.douglas.model.Exchange;
+import br.com.douglas.repository.ExchangeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,10 +19,24 @@ public class ExchangeController {
     @Autowired
     InstanceInformationService informationService;
 
+    @Autowired
+    ExchangeRepository repository;
+
     // http://localhost:8000/exchange-service/5/USD/BRL
     @GetMapping(value = "/{amount}/{from}/{to}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Exchange getExchange(@PathVariable("amount") BigDecimal amount,@PathVariable("from") String from,@PathVariable("to") String to)
     {
-        return new Exchange(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, "PORT " + informationService.retrieveServerPort());
+        Exchange exchange = repository.findByFromAndTo(from, to);
+
+        if (exchange == null)
+            throw new RuntimeException("Currency Unsupported!");
+
+        BigDecimal conversionFactor = exchange.getConversionFactor();
+        BigDecimal convertedValue = conversionFactor.multiply(amount);
+
+        exchange.setConvertedValue(convertedValue);
+        exchange.setEnvironment("PORT: " + informationService.retrieveServerPort());
+
+        return exchange;
     }
 }
